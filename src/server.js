cc3321bf9bd912837dbc592f7f0ecb95baf886ec
romingoon <romingoon@gmail.com>
2022-10-27 11:43:02 +0900

    noom 클론 코딩/ 실시간 챗

---
+++ src/server.js
@@ -0,0 +1,38 @@
+import http from 'http';
+import SocketIO from 'socket.io';
+import express from 'express';
+import path from 'path';
+
+const app = express();
+
+app.set('view engine', 'pug');
+app.set('views', __dirname + '/views');
+app.use('/public', express.static(path.join(__dirname + '/public')));
+
+app.get('/', (req, res) => res.render('home'));
+app.get('/*', (req, res) => res.redirect('/'));
+
+const httpServer = http.createServer(app);
+const wsServer = SocketIO(httpServer);
+
+wsServer.on('connection', (socket) => {
+  socket['nickname'] = 'Anon';
+  socket.on('enter_room', (roomName, done) => {
+    done();
+    socket.join(roomName);
+    socket.to(roomName).emit('welcome', socket.nickname);
+  });
+  socket.on('disconnecting', () => {
+    socket.rooms.forEach((room) =>
+      socket.to(room).emit('bye', socket.nickname),
+    );
+  });
+  socket.on('new_message', (msg, room, done) => {
+    socket.to(room).emit('new_message', `${socket.nickname} : ${msg}`);
+    done();
+  });
+  socket.on('nickname', (nickname) => (socket['nickname'] = nickname));
+});
+
+const handleListen = () => console.log('Listening on http://localhost:3000');
+httpServer.listen(3000, handleListen);
